b9df70cbe152bf03a8c6f4018d1cfffa8ec4e603
Sebron <sebron@gmail.com>
2024-07-12 05:58:33 -0400

    oh these late night commits..

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Splinterd Humanity Twitch App
 
-An app to run a chat bot, alerts, and various other features for streaming. 
-
+An app to run a chat bot, alerts, and various other features for streaming.
 
 <sub>this is for learning and not intended for other streamers to use, yet.</sub>
--- app.js
+++ app.js
@@ -5,26 +5,28 @@ const { Gtk } = imports.gi;
 
 Gtk.init(null);
 
-const box = new Gtk.Box ({ orientation: Gtk.Orientation.VERTICAL });
+const box = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL });
 
-const entry = new Gtk.Entry({
-    buffer: new Gtk.EntryBuffer()
+const entry = new Gtk.TextView({
+  buffer: new Gtk.TextBuffer(),
 });
 
 box.add(entry);
 
 const button = new Gtk.Button({
-    label: 'Enter'
+  label: "Enter",
 });
 
-button.connect('clicked', () => {
-    log('Entered in the entry: ' + entry.get_buffer().text);
+button.connect("clicked", () => {
+  log("Entered in the entry: " + entry.get_buffer().text);
 });
 
 box.add(button);
 
 const win = new Gtk.Window({ defaultHeight: 600, defaultWidth: 800 });
-win.connect('destroy', () => { Gtk.main_quit(); });
+win.connect("destroy", () => {
+  Gtk.main_quit();
+});
 win.add(box);
 win.show_all();
 
